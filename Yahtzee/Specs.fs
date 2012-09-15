2250c61a790d6adedafb60402e78a79b7fc49dcf
Simon Skov Boisen <simon@skov-boisen.dk>
2012-09-15 22:20:36 +0200

    Started implementing the kata using NaturalSpec

---
+++ Yahtzee/Specs.fs
@@ -0,0 +1,26 @@
+﻿module Yahtzee.Specs
+
+open NaturalSpec
+open Yahtzee.Model
+
+// Utils
+let placed on combination roll =
+    printMethod combination
+    calcScore combination roll
+
+let on = ()
+
+
+[<Scenario>]
+let ``Given 1, 1, 2, 4, 4 placed on fours gives 8 points.``() =
+  Given [1; 1; 2; 4; 4]
+    |> When placed on Fours
+    |> It should equal 8
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 1, 2, 4, 4 placed on ones gives 2 points.``() =
+  Given [1; 1; 2; 4; 4]
+    |> When placed on Ones
+    |> It should equal 2
+    |> Verify
---
+++ Yahtzee/Yahtzee.fs
@@ -0,0 +1,12 @@
+﻿module Yahtzee.Model
+
+type Roll = int * int * int * int * int
+
+type Combination =
+    | Ones
+    | Fours
+
+let calcScore combination roll =
+    match combination with
+        | Ones  -> 2
+        | Fours -> 8
