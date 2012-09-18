e52a179d9a2f714e119b8fdc16354f5979e8256c
Simon Skov Boisen <simon@skov-boisen.dk>
2012-09-18 16:20:52 +0200

    Implemented the rest of the spec

--- Yahtzee/Specs.fs
+++ Yahtzee/Specs.fs
@@ -10,17 +10,137 @@ let placed on combination roll =
 
 let on = ()
 
+[<Scenario>]
+let ``Given 1, 1, 1, 4, 4 placed on ones gives 3 points.``() =
+  Given [1; 1; 1; 4; 4]
+    |> When placed on Ones
+    |> It should equal 3
+    |> Verify
 
 [<Scenario>]
-let ``Given 1, 1, 2, 4, 4 placed on fours gives 8 points.``() =
-  Given [1; 1; 2; 4; 4]
+let ``Given 2, 2, 2, 4, 4 placed on ones gives 0 points.``() =
+  Given [2; 2; 2; 4; 4]
+    |> When placed on Ones
+    |> It should equal 0
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 2, 2, 2, 4 placed on twos gives 6 points.``() =
+  Given [1; 2; 2; 2; 4]
+    |> When placed on Twos
+    |> It should equal 6
+    |> Verify
+
+[<Scenario>]
+let ``Given 3, 3, 3, 2, 4 placed on threes gives 9 points.``() =
+  Given [3; 3; 3; 2; 4]
+    |> When placed on Threes
+    |> It should equal 9
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 1, 4, 4, 4 placed on fours gives 12 points.``() =
+  Given [1; 1; 4; 4; 4]
     |> When placed on Fours
+    |> It should equal 12
+    |> Verify
+    
+[<Scenario>]
+let ``Given 1, 1, 4, 4, 4 placed on fives gives 0 points.``() =
+  Given [1; 1; 4; 4; 4]
+    |> When placed on Fives
+    |> It should equal 0
+    |> Verify
+    
+[<Scenario>]
+let ``Given 6, 1, 4, 6, 4 placed on sixes gives 12 points.``() =
+  Given [6; 1; 4; 6; 4]
+    |> When placed on Sixes
+    |> It should equal 12
+    |> Verify
+    
+[<Scenario>]
+let ``Given 1, 1, 3, 4, 4 placed on pair gives 8 points.``() =
+  Given [1; 1; 3; 4; 4]
+    |> When placed on Pair
     |> It should equal 8
     |> Verify
 
 [<Scenario>]
-let ``Given 1, 1, 2, 4, 4 placed on ones gives 2 points.``() =
-  Given [1; 1; 2; 4; 4]
-    |> When placed on Ones
-    |> It should equal 2
+let ``Given 1, 2, 3, 4, 5 placed on pair gives 0 points.``() =
+  Given [1; 2; 3; 4; 5]
+    |> When placed on Pair
+    |> It should equal 0
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 1, 2, 3, 3 placed on two pair gives 8 points.``() =
+  Given [1; 1; 2; 3; 3]
+    |> When placed on TwoPairs
+    |> It should equal 8
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 2, 3, 3, 3 placed on two pair gives 0 points.``() =
+  Given [1; 2; 3; 3; 3]
+    |> When placed on TwoPairs
+    |> It should equal 0
+    |> Verify
+
+[<Scenario>]
+let ``Given 3, 3, 3, 4, 5 placed on three of a kind gives 9 points.``() =
+  Given [3; 3; 3; 4; 5]
+    |> When placed on ThreeOfAKind
+    |> It should equal 9
+    |> Verify
+
+[<Scenario>]
+let ``Given 2, 2, 2, 2, 5 placed on four of a kind gives 8 points.``() =
+  Given [2; 2; 2; 2; 5]
+    |> When placed on FourOfAKind
+    |> It should equal 8
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 2, 3, 4, 5 placed on small straight gives 15 points.``() =
+  Given [1; 2; 3; 4; 5]
+    |> When placed on SmallStraight
+    |> It should equal 15
+    |> Verify
+
+[<Scenario>]
+let ``Given 2, 3, 4, 5, 6 placed on large straight gives 20 points.``() =
+  Given [2; 3; 4; 5; 6]
+    |> When placed on LargeStraight
+    |> It should equal 20
+    |> Verify
+
+[<Scenario>]
+let ``Given 1, 1, 2, 2, 2 placed on full house gives 8 points.``() =
+  Given [1; 1; 2; 2; 2]
+    |> When placed on FullHouse
+    |> It should equal 8
+    |> Verify
+
+[<Scenario>]
+let ``Given 4, 4, 4, 4, 4 placed on full house gives 0 points.``() =
+  Given [4; 4; 4; 4; 4]
+    |> When placed on FullHouse
+    |> It should equal 0
+    |> Verify
+
+
+[<Scenario>]
+let ``Given 4, 4, 4, 4, 4 placed on yahtzee gives 50 points.``() =
+  Given [4; 4; 4; 4; 4]
+    |> When placed on Yahtzee
+    |> It should equal 50
+    |> Verify
+
+
+[<Scenario>]
+let ``Given 4, 4, 4, 3, 4 placed on chance gives 19 points.``() =
+  Given [4; 4; 4; 3; 4]
+    |> When placed on Chance
+    |> It should equal 19
     |> Verify
--- Yahtzee/Yahtzee.fs
+++ Yahtzee/Yahtzee.fs
@@ -4,9 +4,67 @@ type Roll = int * int * int * int * int
 
 type Combination =
     | Ones
+    | Twos
+    | Threes
     | Fours
+    | Fives
+    | Sixes
+    | Pair
+    | TwoPairs
+    | ThreeOfAKind
+    | FourOfAKind
+    | SmallStraight
+    | LargeStraight
+    | FullHouse
+    | Yahtzee
+    | Chance
 
 let calcScore combination roll =
+    let roll = roll |> List.sort
+    let calcForNumberCombination number roll =
+        roll |> Seq.filter (fun f -> f = number) |> Seq.sum
+    let findGroupsOf number roll =
+        roll |> Seq.groupBy id 
+             |> Seq.filter (fun f -> (snd f) |> Seq.length >= number) 
+    let findGroupsOfExact number roll =
+        roll |> Seq.groupBy id 
+             |> Seq.filter (fun f -> (snd f) |> Seq.length = number)
+    let collectSum list =
+        list |> Seq.sumBy (fun f -> snd f |> Seq.sum)
     match combination with
-        | Ones  -> 2
-        | Fours -> 8
+        | Ones  -> roll |> calcForNumberCombination 1
+        | Twos  -> roll |> calcForNumberCombination 2
+        | Threes -> roll |> calcForNumberCombination 3
+        | Fours -> roll |> calcForNumberCombination 4
+        | Fives -> roll |> calcForNumberCombination 5
+        | Sixes -> roll |> calcForNumberCombination 6
+        | Pair -> let pairs = findGroupsOf 2 roll
+                  if pairs |> Seq.length > 0 then
+                    pairs |> Seq.maxBy fst |> snd |> Seq.sum
+                  else 
+                    0
+        | TwoPairs -> let pairs = findGroupsOf 2 roll
+                      if pairs |> Seq.length > 1 then
+                        pairs |> collectSum
+                      else
+                        0 
+        | ThreeOfAKind -> roll  |> findGroupsOf 3 |> collectSum
+        | FourOfAKind -> roll |> findGroupsOf 4 |> collectSum
+        | SmallStraight -> match roll with
+                            | [1;2;3;4;5] -> 15
+                            | _ -> 0
+        | LargeStraight -> match roll with
+                            | [2;3;4;5;6] -> 20
+                            | _ -> 0
+        | FullHouse ->  let pair = findGroupsOfExact 2 roll
+                        let triple = findGroupsOfExact 3 roll
+                        if pair |> Seq.length = 1 then
+                            (pair |> collectSum) + (triple |> collectSum)
+                        else
+                            0
+        | Yahtzee ->  let yahtzee = findGroupsOfExact 5 roll
+                      if yahtzee |> Seq.length = 1 then
+                        50
+                      else
+                        0
+        | Chance -> roll |> Seq.sum
